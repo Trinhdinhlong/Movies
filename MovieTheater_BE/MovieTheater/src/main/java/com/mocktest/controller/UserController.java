d5bf3f4f9994f1fefca4ac44aeba091af6b2b8fc
Trinhdinhlong <trinhdinhlong2003@gmail.com>
2024-03-06 00:28:20 +0700

    Controller

--- MovieTheater_BE/MovieTheater/src/main/java/com/mocktest/controller/UserController.java
+++ MovieTheater_BE/MovieTheater/src/main/java/com/mocktest/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @CrossOrigin("*")
 @RestController
-@RequestMapping("/api/teams")
+@RequestMapping("/api/user")
 public class UserController {
     @Autowired
     private UserService userService;
