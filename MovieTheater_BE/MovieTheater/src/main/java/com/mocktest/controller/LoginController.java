5ec3674d3d0324304ef6eb8b8b5b90ca6654f0a9
Trinhdinhlong <trinhdinhlong2003@gmail.com>
2024-03-07 13:38:20 +0700

    Update Controller

--- MovieTheater_BE/MovieTheater/src/main/java/com/mocktest/controller/LoginController.java
+++ MovieTheater_BE/MovieTheater/src/main/java/com/mocktest/controller/LoginController.java
@@ -1,15 +1,9 @@
 package com.mocktest.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mocktest.dto.UserDto;
 import com.mocktest.jsons.UserJson;
-
-import com.mocktest.payload.ResponseData;
-import com.mocktest.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
--- MovieTheater_BE/MovieTheater/src/main/java/com/mocktest/services/UserService.java
+++ MovieTheater_BE/MovieTheater/src/main/java/com/mocktest/services/UserService.java
@@ -1,7 +1,6 @@
 package com.mocktest.services;
 
 import com.mocktest.dto.UserDto;
-import com.mocktest.entities.User;
 
 public interface UserService extends BaseService<UserDto> {
     UserDto getByUserName(String username);
