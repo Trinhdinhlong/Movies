e276cbab8ab9e0734d27b51cfa06a0ca2b66bbbb
nguyen2410 <ducnguyenbernardine@gmail.com>
2024-03-15 21:30:59 +0700

    Added showtime and selecting seats

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,3 @@
-"use client";
 import Image from "next/image";
 import logo from "@/public/logo.svg";
 import dropdown from "@/public/dropdown.svg";
@@ -7,47 +6,39 @@ import user from "@/public/User.svg";
 import logout from "@/public/Logout.svg";
 
 export default function Header({ handleOpen }: any) {
+  
   function handleOpenPopup() {
     handleOpen();
   }
 
   return (
-    <div className="w-full h-full top-[0]">
-      <div className="z-[0] px-[30px] py-[10px] bg-[#205081] flex flex-row items-center justify-between w-full h-full">
-        <div>
-          <Image src={logo} alt="MovieTheaterLogo" />
+    <div className="z-[0] w-full h-full flex flex-row px-5 gap-5 justify-between">
+      <div className="flex flex-col justify-center">
+        <Image src={logo} alt="MovieTheaterLogo" />
+      </div>
+      <div className="h-full flex flex-row items-center justify-between gap-14">
+        <span className="block">Showtime</span>
+        <div className="flex flex-row">
+          <button className="flex flex-row items-center justify-center gap-2 cursor-pointer" onClick={() => handleOpenPopup()}>
+            <span>Movies</span>
+            <Image src={dropdown} alt="DropDown Logo" />
+          </button>
         </div>
-        <div className="flex flex-row items-center gap-14">
-          <span>Showtime</span>
-          <div className="overflow-hidden">
-            <button
-              className="flex flex-row gap-3 cursor-pointer items-center"
-              onClick={() => handleOpenPopup()}
-            >
-              <span>Movies</span>
-              <Image src={dropdown} alt="DropDown Logo" />
-            </button>
+        <div className="flex flex-row bg-white gap-2 p-1 px-2 items-center justify-center rounded-[10px] text-black">
+          <input type="text" placeholder="Search" className="outline-none"/>
+          <div className="">
+            <Image src={search} alt="" />
           </div>
-          <form className="flex flex-row h-[2rem] justify-center items-center bg-white rounded-[10px]">
-            <input
-              type="text"
-              placeholder="Search"
-              className=" rounded-[10px] px-4 text-black h-full outline-none"
-            />
-            <div className="border-solid border-l-[0.5px] border-l-[#EFF0F3] h-full flex items-center p-2">
-              <Image src={search} alt="" />
-            </div>
-          </form>
         </div>
-        <div className="flex flex-row gap-10">
-          <div className="cursor-pointer flex flex-row gap-2">
-            <Image src={user} alt="" />
-            <span>Welcome, julientlam</span>
-          </div>
-          <div className="cursor-pointer flex flex-row gap-2">
-            <Image src={logout} alt="" />
-            <span>Logout</span>
-          </div>
+      </div>
+      <div className="flex flex-row items-center justify-center gap-10">
+        <div className="flex flex-row gap-3 items-center justify-center">
+          <Image src={user} alt="" />
+          <span>Welcome, julientlam</span>
+        </div>
+        <div className="flex flex-row gap-3 items-center justify-center">
+          <Image src={logout} alt="" />
+          <span>Logout</span>
         </div>
       </div>
     </div>
