efe1997bae7df9ea9563956c2b0ec6beddbd075d
Trinhdinhlong <trinhdinhlong2003@gmail.com>
2024-03-18 22:25:02 +0700

    Update RoomService

--- MovieTheater_BE/MovieTheater/src/main/java/com/mocktest/services/RoomService.java
+++ MovieTheater_BE/MovieTheater/src/main/java/com/mocktest/services/RoomService.java
@@ -7,7 +7,6 @@ import com.mocktest.exceptions.NotFoundException;
 import com.mocktest.repository.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
