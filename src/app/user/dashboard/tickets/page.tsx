8ccd3f7fd4e22271b91e18336adba52dacd2bb24
Trinhdinhlong <trinhdinhlong2003@gmail.com>
2024-03-19 01:11:35 +0700

    Update page.tsx

--- src/app/user/dashboard/tickets/page.tsx
+++ src/app/user/dashboard/tickets/page.tsx
@@ -4,19 +4,16 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 interface Ticket {
-    name: string;
-    amount: number;
-    date: string;
-    price: string;
-    status: string;
+  movieNameVN: string;
+  totalAmount: number;
+  startTime: string;
+  ticketType: string;
 }
 
 export default function Home() {
   const [tickets, setTickets] = useState<Ticket[]>([]);
   useEffect(() => {
-    axios.get("http://localhost:8080/api/ticket/booked").then(response => {
-      setTickets(response.data)
-    })
+    axios.get("http://localhost:8080/api/ticket/booked")
   }, []);
 
   return (
@@ -54,13 +51,13 @@ export default function Home() {
             </thead>
             <tbody>
               {tickets.map((ticket, index) => (
-                <tr key={ticket.name} className="border-b-[1px] border-solid border-[#BEC8CF]">
+                <tr key={ticket.movieNameVN} className="border-b-[1px] border-solid border-[#BEC8CF]">
                   <td className="py-[10px]">{index}</td>
-                  <td className="py-[10px]">{ticket.name}</td>
-                  <td className="py-[10px]">{ticket.date}</td>
-                  <td className="py-[10px]">{ticket.price}</td>
+                  <td className="py-[10px]">{ticket.movieNameVN}</td>
+                  <td className="py-[10px]">{ticket.startTime}</td>
+                  <td className="py-[10px]">{ticket.totalAmount}</td>
                   <td className="py-[10px] text-[#2036F8]">
-                    {ticket.status}
+                    {ticket.ticketType}
                   </td>
                 </tr>
               ))}
